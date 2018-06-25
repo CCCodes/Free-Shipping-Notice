f4f629107fbae1496434bf3952d75b1f64e88a71
Caitlin <caitlinchou@gmail.com>
2018-06-25 12:51:47 -0400

    remove #

--- free-shipping-notice.php
+++ free-shipping-notice.php
@@ -72,7 +72,7 @@ function fsn_options_page() {
             <label for="color-picker">Color</label>
             <input type="text" class="color-picker" name="fsn-options[highlight-color]" id="color-picker" value="<?php echo get_option('fsn-options[highlight-color]')?>" />
             <label for="fsn-shipping-min">Free Shipping Minimum ($)</label>
-            <input type="number" name="fsn-options[shipping-min]" id='fsn-shipping-min' value="#<?php echo get_option('fsn-options[shipping-min]');?>" />
+            <input type="number" name="fsn-options[shipping-min]" id='fsn-shipping-min' value="<?php echo get_option('fsn-options[shipping-min]');?>" />
             <?php submit_button(); ?>
         </form>
     </div>
