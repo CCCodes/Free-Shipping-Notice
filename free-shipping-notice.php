c3419d6fcb6068785ab2a58fda430467fdaa59e4
Caitlin <caitlinchou@gmail.com>
2018-06-25 12:29:22 -0400

    color picker test

--- free-shipping-notice.php
+++ free-shipping-notice.php
@@ -70,7 +70,7 @@ function fsn_options_page() {
             <?php settings_fields('fsn_settings'); ?>
             <?php do_settings_sections('fsn_settings'); ?>
             <label for="fsn-color-picker">Color</label>
-            <input type="text" class="color-picker" name="fsn-options[highlight-color]" id='fsn-color-picker' value="#<?php echo get_option('fsn-options[highlight-color]');?>" placeholder="#ff0000" />
+            <input type="text" class="color-picker" name="fsn-options[highlight-color]" id='fsn-color-picker' value="#ff0000" placeholder="#ff0000" />
             <label for="fsn-shipping-min">Free Shipping Minimum ($)</label>
             <input type="number" name="fsn-options[shipping-min]" id='fsn-shipping-min' value="#<?php echo get_option('fsn-options[shipping-min]');?>" placeholder="50" />
             <?php submit_button(); ?>
