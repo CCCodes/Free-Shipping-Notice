cbdf523a57cf0fe6c7015d0abe839bffcb065df6
Caitlin <caitlinchou@gmail.com>
2018-06-25 20:08:05 -0400

    debug option type

--- free-shipping-notice.php
+++ free-shipping-notice.php
@@ -97,7 +97,7 @@ function fsn_options_page() {
                 $countries_obj = new WC_Countries();
                 $countries = $countries_obj->__get('countries');
                 foreach($countries as $country) {
-                    echo "<option ".(in_array($country,$option)==1 ? "selected" : "")."'>$country</option>";
+                    echo "<option ".(in_array($country,$option)==True ? "selected" : "")."'>$country</option>";
                 }?>
             </select>
             <?php submit_button(); ?>
